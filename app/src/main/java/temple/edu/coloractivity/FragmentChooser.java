3ab82b0279e9b81e582f235a9817ee3220346dbd
KnoxyFresh <46945944+knoxyfresh@users.noreply.github.com>
2019-10-17 12:44:32 -0400

    made arrays for colors for fragment

---
+++ app/src/main/java/temple/edu/coloractivity/FragmentChooser.java
@@ -0,0 +1,46 @@
+package temple.edu.coloractivity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentChooser extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ColorAdapter coloradapter;
+
+    public FragmentChooser() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+        String[] colors = getResources().getStringArray(R.array.colors);
+        String[] colornames = getResources().getStringArray(R.array.colornames);
+        coloradapter =new ColorAdapter(this,colornames,colors);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment_chooser, container, false);
+    }
+
+}
--- app/src/main/java/temple/edu/coloractivity/MainActivity.java
+++ app/src/main/java/temple/edu/coloractivity/MainActivity.java
@@ -1,41 +1,42 @@
 package temple.edu.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
-import android.content.res.Resources;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.FrameLayout;
 import android.widget.Spinner;
 
-public class PaletteActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
+
+    FragmentChooser chooser;
+    FragmentColor showcolor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner myspinner = findViewById(R.id.spinner);
-        String colors[] = getResources().getStringArray(R.array.colors);
+        //Spinner myspinner = findViewById(R.id.spinner);
+        /*String colors[] = getResources().getStringArray(R.array.colors);
         String colornames[] = getResources().getStringArray(R.array.colornames);
-        ColorAdapter ca = new ColorAdapter(PaletteActivity.this,colornames,colors);
+        ColorAdapter ca = new ColorAdapter(MainActivity.this,colornames,colors);
 
         myspinner.setAdapter(ca);
         myspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if(i>0){
-                    Intent intent = new Intent(getApplicationContext(), CanvasActivity.class);
+
                     ColorAdapter adapter = (ColorAdapter) adapterView.getAdapter();
                     String colorname = adapter.getColorName(i);
                     String color = adapter.getItem(i).toString();
                     Log.e("color name",color);
-                    intent.putExtra("color",color);
-                    intent.putExtra("colorname",colorname);
-                    startActivity(intent);
+
                 }
             }
 
@@ -43,7 +44,7 @@ public class PaletteActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
-        });
+        });*/
     }
 
 
